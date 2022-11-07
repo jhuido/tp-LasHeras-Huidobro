2ddbade9497c9789eac0b9972b9d2bb561624ea4
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 12:36:13 -0300

    'TDD'

--- test/paquete.test.js
+++ test/paquete.test.js
@@ -1,12 +1,13 @@
 const Paquete=require('../src/Paquete.js');
 
 test("Crear Paquete",()=>{
-    var paquete=new Paquete("Local","Destino",[["Remera",3],["Pantalon",1]]);
+    var paquete=new Paquete("Local","Destino",[["Remera",3],["Pantalon",1]],5);
     expect(paquete.ubicacionActual).toBe("Local");
     expect(paquete.destino).toBe("Destino");
     expect(paquete.productos.length).toBe(2);
+    expect(paquete.urgencia).toBe(5);
 })
 
 test("Validar por lo menos un producto x paquete",()=>{
-    expect(()=>{var paquete=new Paquete("Local","Destino",[]);}).toThrow("Cada paquete debe tener por lo menos un producto");
+    expect(()=>{var paquete=new Paquete("Local","Destino",[],5);}).toThrow("Cada paquete debe tener por lo menos un producto");
 })
