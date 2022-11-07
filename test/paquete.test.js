a1f2dfebca97436b1a1bb6bd86749640c2f3edfa
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 12:13:48 -0300

    'TDD'

--- test/paquete.test.js
+++ test/paquete.test.js
@@ -1,6 +1,7 @@
 const Paquete=require('../src/Paquete.js');
 
 test("Crear Paquete",()=>{
-    var paquete=new Paquete();
+    var paquete=new Paquete("Destino");
     expect(paquete.ubicacion()).toBe("Local");
+    expect(paquete.destino).toBe("Destino");
 })
