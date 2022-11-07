9ba3221145ee94d78ae3cf8ca8070739af81f820
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 12:43:28 -0300

    'TDD'

--- src/Paquete.js
+++ src/Paquete.js
@@ -1,17 +1,22 @@
 
 
 function Paquete(ubicacionActual,destino,productos,urgencia){
-    if(productos.length==0){
-        throw new Error("Cada paquete debe tener por lo menos un producto");
-    }
-    if(urgencia<0){
-        throw new Error("La urgencia del paquete debe ser mayor a cero");
-    }
+
+    validarParametros();
 
     this.ubicacionActual=ubicacionActual;
     this.destino=destino;
     this.productos=productos;
     this.urgencia=urgencia;
+
+    function validarParametros(){
+        if(productos.length==0){
+            throw new Error("Cada paquete debe tener por lo menos un producto");
+        }
+        if(urgencia<0){
+            throw new Error("La urgencia del paquete debe ser mayor a cero");
+        }
+    }
 
 }
 
