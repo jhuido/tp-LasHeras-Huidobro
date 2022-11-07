b0d745c0118e900fa6c76b635c5a5458b62c33c0
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 12:36:43 -0300

    'TDD'

--- src/Paquete.js
+++ src/Paquete.js
@@ -1,6 +1,6 @@
 
 
-function Paquete(ubicacionActual,destino,productos){
+function Paquete(ubicacionActual,destino,productos,urgencia){
     if(productos.length==0){
         throw new Error("Cada paquete debe tener por lo menos un producto");
     }
@@ -8,6 +8,7 @@ function Paquete(ubicacionActual,destino,productos){
     this.ubicacionActual=ubicacionActual;
     this.destino=destino;
     this.productos=productos;
+    this.urgencia=urgencia;
 
 }
 
